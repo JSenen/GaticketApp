4405f918f7272fb03720d3b0c535311d1b072b21
Juan Senen <juansenen@hotmail.com>
2023-11-16 20:38:50 +0100

    Api file BASE_URL conection

---
+++ app/src/main/java/com/juansenen/gaticketapp/api/Constantes.java
@@ -0,0 +1,10 @@
+package com.juansenen.gaticketapp.api;
+
+/**
+ * Constantes donde se almacena la dirección base de la Api.
+ * (Para conexion local colocar la ip del pc)
+ */
+public class Constantes {
+
+    public static String BASE_URL ="http://192.168.1.130/";
+}
